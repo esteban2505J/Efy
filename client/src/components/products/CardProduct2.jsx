3b59388177b7c8f3b04b68f24cc24bad8b64e28f
esteban2505J <juane.ramirezt@uvirtual.edu.co>
2024-04-19 22:36:31 -0500

    revoled conflics in cardProduct2

--- client/src/components/products/CardProduct2.jsx
+++ client/src/components/products/CardProduct2.jsx
@@ -1,40 +1,27 @@
-<<<<<<< HEAD
 import React, { useEffect } from "react";
 import { Card, CardBody, CardFooter, Image, link } from "@nextui-org/react";
 import { Link } from "react-router-dom";
-=======
-import React from "react";
-import { Card, CardBody, CardFooter} from "@nextui-org/react";
->>>>>>> 21e58ecda5969bd2254aebd4a1d61cf65b9cc0ee
 import { useNavigate } from "react-router-dom";
 import PaginationNav from "../tienda/PaginationNav";
 import { useProduct } from "../../context/ProductContext";
 
-import { productList } from "./productData";
-
 export default function CardProduct2() {
   const navigate = useNavigate();
-<<<<<<< HEAD
+
   const { products, updateProduct } = useProduct();
   const list = products;
-=======
->>>>>>> 21e58ecda5969bd2254aebd4a1d61cf65b9cc0ee
 
   return (
     <>
       <div className="gap-2 grid grid-cols-2 sm:grid-cols-4">
-<<<<<<< HEAD
         {list.map((item) => (
-=======
-        {productList.map((item, index) => (
->>>>>>> 21e58ecda5969bd2254aebd4a1d61cf65b9cc0ee
           <Card
             isBlurred="true"
             shadow="sm"
             key={item._id}
             isPressable={true}
             // En CardProduct2
-<<<<<<< HEAD
+
             onPress={() => {
               try {
                 navigate(`/product/${item._id}`);
@@ -42,11 +29,7 @@ export default function CardProduct2() {
                 console.log(error);
               }
             }}
-=======
-            onClick={() =>
-              navigate(`/product/${item.title}`)
-            }
->>>>>>> 21e58ecda5969bd2254aebd4a1d61cf65b9cc0ee
+            onClick={() => navigate(`/product/${item.title}`)}
           >
             <CardBody className="overflow-visible p-0 ">
               <img
