c2c5e6eaab8e1a2006ae3f1087192d44e31ebd12
esteban2505J <juane.ramireztqvirtual.edu.co>
2024-06-18 21:05:00 -0500

    update product model

--- src/models/product.model.js
+++ src/models/product.model.js
@@ -2,21 +2,18 @@ import mongoose from "mongoose";
 
 const { Schema, model } = mongoose;
 
-// Esta función es para crear el esquema en la base de datos
 const productSchema = new Schema({
   title: { type: String, required: true },
-  house: { type: String }, // Corregido
   description: { type: String, required: true },
   referenceImage: {
     publicId: String,
     secureUrl: String,
   },
-  composition: {
-   type: String, required:true
-  },
   categories: [{ type: String, required: true }],
   typeProduct: { type: String, required: true },
-});
+  price : {type : String, required:true},
+  attributes: { type: Map, of: String ,default :{}}}, {timestamps :true}
+);
 
 const Product = model("Product", productSchema);
 
